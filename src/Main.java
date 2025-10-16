94fe9b997fc696715db5b2c155ae821a99b1f053
Imxantek <antekaminski@gmail.com>
2025-10-16 23:37:19 +0200

    zadanie 8 oba podpunkty. odpowiedzi 336 oraz 1131. final commit.

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,6 @@
 import java.io.*;
 import java.util.ArrayList;
-import java.util.Comparator;
-import java.util.HashMap;
+
 
 public class Main {
     static ArrayList<String> t=new ArrayList<>();
@@ -15,7 +14,7 @@ public class Main {
         t.removeLast();
     }
 
-    static Antinode calculateAntinodes(int x1, int y1, int x2, int y2, int x_max, int y_max) {
+    public static Antinode calculateAntinodes1(int x1, int y1, int x2, int y2, int x_max, int y_max) {
         if(x1==x2 && y1==y2){
             return null;
         }
@@ -30,18 +29,28 @@ public class Main {
         }
         return null;
     }
-
-    public static void main(String[] args)  {
-
-        try{
-            load(t);
-        }catch(IOException e){
-            e.printStackTrace();
+    public static ArrayList<Antinode> calculateAntinodes2(int x1, int y1, int x2, int y2, int x_max, int y_max) {
+        ArrayList<Antinode> antinodes = new ArrayList<>();
+        if(x1==x2 && y1==y2){
+            return null;
         }
+        int vx, vy, x_res, y_res;
+        vx = x2-x1;
+        vy = y2-y1;
+        x_res = x1+vx;
+        y_res = y1+vy;
+        while(x_max > x_res && y_max > y_res && x_res >= 0 && y_res >= 0){
+            antinodes.add(new Antinode(x_res, y_res));
+            x_res +=vx;
+            y_res +=vy;
+        }
+        return antinodes;
+    }
+
+    public static void loadSignals(ArrayList<signal>signals){
         String line;
-        int x_max=t.size(), y_max=t.getFirst().length();
+
 //        System.out.println(x_max + " " + y_max);
-        ArrayList<signal> signals = new ArrayList<>();
 
         for(int i=0; i<t.size(); i++){
             line=t.get(i);
@@ -53,29 +62,70 @@ public class Main {
                 }
             }
         }
-        char c;
-        int count=0;
+    }
+
+    public static void ex1(){
+        ArrayList<signal> signals = new ArrayList<>();
+        int x_max=t.size(), y_max=t.getFirst().length();
+        loadSignals(signals);
+        char type;
         ArrayList<Antinode> antinodes = new ArrayList<>();
         for(int i=0; i<signals.size(); i++){
             signal src = signals.get(i);
-            c=src.getType();
-            for(int j=0; j<signals.size(); j++){
-                signal dest = signals.get(j);
-                Antinode antinode=calculateAntinodes(src.getX(), src.getY(), dest.getX(), dest.getY(), x_max, y_max);
-                if(c==dest.getType() && antinode!=null){
+            type=src.getType();
+            for (signal dest : signals) {
+                Antinode antinode = calculateAntinodes1(src.getX(), src.getY(), dest.getX(), dest.getY(), x_max, y_max);
+                if (type == dest.getType() && antinode != null) {
                     boolean found = false;
-                    for(Antinode a : antinodes){
-                        if(a.getX()==antinode.getX() && a.getY()==antinode.getY()){
+                    for (Antinode a : antinodes) {
+                        if (a.getX() == antinode.getX() && a.getY() == antinode.getY()) {
                             found = true;
+                            break;
                         }
                     }
-                    if(!found){
-                        count++;
+                    if (!found) {
                         antinodes.add(antinode);
                     }
                 }
             }
         }
-        System.out.println(count);
+        System.out.println(antinodes.size());
+    }
+
+    public static void ex2(){
+        ArrayList<signal> signals = new ArrayList<>();
+        ArrayList<Antinode> totalAntinodes = new ArrayList<>();
+        int x_max=t.size(), y_max=t.getFirst().length();
+        char type;
+        loadSignals(signals);
+
+        for(int i=0; i<signals.size(); i++){
+            signal src = signals.get(i);
+            type=src.getType();
+            for (signal dest : signals) {
+                if (type == dest.getType()) {
+                    ArrayList<Antinode> newAntinodes = calculateAntinodes2(src.getX(), src.getY(), dest.getX(), dest.getY(), x_max, y_max);
+                    if (newAntinodes != null) {
+                        for (Antinode a : totalAntinodes) {
+                            newAntinodes.removeIf(b -> a.getX() == b.getX() && a.getY() == b.getY());
+                        }
+                        totalAntinodes.addAll(newAntinodes);
+                    }
+                }
+            }
+        }
+        System.out.println(totalAntinodes.size());
+    }
+
+    public static void main(String[] args)  {
+
+        try{
+            load(t);
+        }catch(IOException e){
+            e.printStackTrace();
+        }
+        ex1();
+        ex2();
+
     }
 }
