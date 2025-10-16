296aff7503c23b58ffaa0c8b374ed4c668feba6e
Imxantek <antekaminski@gmail.com>
2025-10-16 12:28:44 +0200

    komit 2

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,16 @@
+import java.io.*;
+import java.util.ArrayList;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new FileReader("src/aoc8_test.txt"));
+        String line=br.readLine();
+        ArrayList<String> t= new ArrayList<>();
+
+        while(line!=null){
+            line=br.readLine();
+            t.add(line);
+        }
+
     }
 }
